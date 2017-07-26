826f7648b2c8535c6f1de6dae2622eb134349ad6
James Vanderhyde <vanderhyde@sxu.edu>
2017-07-26 18:58:54 -0500

    Task 3

--- shutthebox.clj
+++ shutthebox.clj
@@ -1,1 +1,11 @@
 ;;Monte Carlo tree search for Shut the Box
+
+;;Task 3
+(defn dice-roll []
+  (+ 2 (rand-int 6) (rand-int 6)))
+(dice-roll)
+
+(def sample-box
+  (set (range 1 10)))
+(disj sample-box 1 3 6)
+
