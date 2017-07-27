c3686c2ab60ae6aa1db67d105aec44325cb86a70
James Vanderhyde <vanderhyde@sxu.edu>
2017-07-26 19:42:58 -0500

    Task 7

--- shutthebox.clj
+++ shutthebox.clj
@@ -22,14 +22,24 @@ sample-box
 (possible-moves sample-box 4)
 (possible-moves sample-box 3)
 
-;;Task 5
-
+;;Task 6
 (defn play-game [box]
   (if (win? box) :win
     (let [roll (dice-roll)
           moves (possible-moves box roll)]
-      (println box roll)
       (if (empty? moves) :loss
         (recur (first moves))))))
 (play-game (disj start-box 1))
 (play-game easy-box)
+
+;;Task 7
+(defn simulate [num-trials]
+  (loop [n num-trials
+         record {:win 0, :loss 0}]
+    (if (zero? n)
+      (double (/ (:win record) (+ (:win record) (:loss record))))
+      (recur (dec n) (update-in record [(play-game (disj start-box 1))] inc)))))
+(simulate 1000000)
+;Probability of winning the simplified game is about 0.02%
+
+
