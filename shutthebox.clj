5041dcecdb782862994b3a1f23ce5c4ef61e8afc
James Vanderhyde <vanderhyde@sxu.edu>
2017-07-26 19:24:49 -0500

    Task 5

--- shutthebox.clj
+++ shutthebox.clj
@@ -7,8 +7,12 @@
 
 (def start-box
   (set (range 1 10)))
-(def sample-box (disj sample-box 1 3 6))
+(def sample-box (disj start-box 1 3 6))
 sample-box
+(def easy-box #{7})
+
+(defn win? [box]
+  (empty? box))
 
 ;;Task 4
 (defn possible-moves [box roll]
@@ -18,3 +22,14 @@ sample-box
 (possible-moves sample-box 4)
 (possible-moves sample-box 3)
 
+;;Task 5
+
+(defn play-game [box]
+  (if (win? box) :win
+    (let [roll (dice-roll)
+          moves (possible-moves box roll)]
+      (println box roll)
+      (if (empty? moves) :loss
+        (recur (first moves))))))
+(play-game (disj start-box 1))
+(play-game easy-box)
