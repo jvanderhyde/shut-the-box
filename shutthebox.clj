595c654f0bfbe38ec3a8e08d4226297646318a2c
James Vanderhyde <vanderhyde@sxu.edu>
2017-07-26 19:07:39 -0500

    Task 4

--- shutthebox.clj
+++ shutthebox.clj
@@ -5,7 +5,16 @@
   (+ 2 (rand-int 6) (rand-int 6)))
 (dice-roll)
 
-(def sample-box
+(def start-box
   (set (range 1 10)))
-(disj sample-box 1 3 6)
+(def sample-box (disj sample-box 1 3 6))
+sample-box
+
+;;Task 4
+(defn possible-moves [box roll]
+  (if (contains? box roll)
+    #{(disj box roll)}
+    #{}))
+(possible-moves sample-box 4)
+(possible-moves sample-box 3)
 
